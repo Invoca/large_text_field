c104b7d7fd7193a2079c0cbb39e1de795c0aea93
Donnie Tognazzini <dtognazzini@invoca.com>
2020-03-05 15:05:55 -0800

    define namespace module before anything else

--- lib/large_text_field.rb
+++ lib/large_text_field.rb
@@ -3,10 +3,10 @@ require "protected_attributes"
 
 require "rails"
 
+module LargeTextField
+  MAX_LENGTH = 5_000_000
+end
+
 require "large_text_field/engine"
 require "large_text_field/owner"
 require "large_text_field/named_text_value"
-
-module LargeTextField
-  MAX_LENGTH = 5_000_000 unless defined?(MAX_LENGTH)
-end
