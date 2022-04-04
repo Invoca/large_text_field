ec05837a9dcdc9eacdf08290b564751250ef4c78
James Ebentier <jebentier@invoca.com>
2022-04-04 16:18:06 +0200

    non-production: update migrations for rails 5+

--- test/dummy/db/migrate/20160217033529_create_libraries.rb
+++ test/dummy/db/migrate/20160217033529_create_libraries.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateLibraries < ActiveRecord::Migration
+class CreateLibraries < ActiveRecord::Migration[4.2]
   def self.up
     create_table :libraries do |t|
       t.string :name, null: false
